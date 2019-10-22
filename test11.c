8b0eff2c33cc85d1058301086e695fc3a269a124
attend996 <56283315+attend996@users.noreply.github.com>
2019-10-22 09:46:50 +0800

    Add files via upload

---
+++ test11.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{float f;
+      printf("enter f:\n");
+
+scanf("%f,&f");
+printf("c is %.2f\n",f=5.0/9*(f-32));                                      
+
+ return  0;
+}
