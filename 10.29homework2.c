9f1b5027f5e19bf967669ed5a55c880a9d0719c7
attend996 <56283315+attend996@users.noreply.github.com>
2019-10-29 10:02:19 +0800

    Add files via upload

---
+++ 10.29homework2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{float a, b,c,d ,all,average;
+scanf("%d%d%d%d,&a&b&c&d");
+all=a+b+c+d;
+average=all/4;
+printf("all is %.1f\n",all);
+printf("average is %.1f\n",average);
+return 0;
+
+}
