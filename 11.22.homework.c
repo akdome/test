31c7b2a60ed21111ed5ffb4b0c78e6d8b4910f5a
attend996 <56283315+attend996@users.noreply.github.com>
2019-11-25 22:15:14 +0800

    Add files via upload

---
+++ 11.22.homework.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{int a, b ,c;
+for(a=0;a<=20;a++);
+for(b=0;b<=33;b++);
+for(c=3;c<=99;c++);
+if(a+b+c==100);
+if(a*5+b*3+c/3==100);
+if(c%3==0);
+printf("%d %d %d\n",a, b, c);
+return 0;}
