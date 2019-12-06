a30acbc91962b9463bd7c4fd9c9b8fbeebb2a414
attend996 <56283315+attend996@users.noreply.github.com>
2019-12-06 09:32:49 +0800

    Add files via upload

---
+++ 12.06.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+/*#include<math.h>
+int isprime(int x);
+int main(void)
+{
+    int i,count=0;
+    for(i=2;i<=100;i++) 
+    {
+        if(isprime(i)==1){
+        printf("%-3d ",i);
+        count++;
+        if(count%5==0){
+            printf("\n");
+        }
+        }
+    }
+    
+    
+return 0;
+}
+int isprime(int x)
+{
+    int i;
+    for(i=2;i<sqrt(x);i++)
+    {
+    if(x%i==0){
+        return 0;
+    }
+    }
+    return 1;
+    
+    
+}*/
+int main()
+{
+    double x,y;
+    scanf("lf%",&x);
+    y=(long)x;
+    printf("lf",y);
+    return 0;}
